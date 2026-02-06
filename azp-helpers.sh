c1ac81055a6105648caaba27f04a005ef94f23af
Alejandro Hernandez Samaniego <alejandro@enedino.org>
2026-02-06 09:39:40 -0700

    native sstate
    Signed-off-by: Alejandro Hernandez Samaniego <alejandro@enedino.org>

--- azp-helpers.sh
+++ azp-helpers.sh
@@ -294,6 +294,9 @@ function create_local_dirs() {
     sudo chown vsts:vsts ${DL_DIR}
     sudo chown vsts:vsts ${SSTATE_MIRRORS_DIR}
     sudo chown vsts:vsts ${DEPLOY_ARTIFACTS_DIR}
+    if [ "$(ls -A ~/base-artifacts)" ]; then
+        mv ~/base-artifacts/* ${SSTATE_DIR}/
+    fi
     check_freespace
 }
 
